92aee0b0c246a8b86d93743a192c75a47270e9f9
AlekSmily <tutun228@gmail.com>
2023-05-24 18:17:34 +0300

    add TCP H2 server in AppConfig class, add application.yaml instead of application.properties, configured data jpa and H2 db

--- src/main/java/com/example/diploma/Main.java
+++ src/main/java/com/example/diploma/Main.java
@@ -1,7 +1,5 @@
 package com.example.diploma;
 
-import com.example.diploma.model.Role;
-import com.example.diploma.model.User;
 import com.example.diploma.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -9,21 +7,17 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Set;
-
 @SpringBootApplication
 @AllArgsConstructor
 public class Main implements ApplicationRunner {
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		userRepository.save(new User("user@gmail.com", "User", "First", "qwerty", Set.of(Role.ROLE_USER)));
-		userRepository.save(new User("admin@gmail.com", "Admin", "First", "qwerty", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
-		System.out.println(userRepository.findAll());
-	}
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(userRepository.findAll());
+    }
 }
