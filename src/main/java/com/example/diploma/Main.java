3a14893bfedd25fa5fb4d327e93a6e4222cb410c
AlekSmily <tutun228@gmail.com>
2023-05-24 21:24:50 +0300

    added BaseEntity class as super class for entity, add in UserRepository two methods for query

--- src/main/java/com/example/diploma/Main.java
+++ src/main/java/com/example/diploma/Main.java
@@ -18,6 +18,7 @@ public class Main implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(userRepository.findAll());
+//        System.out.println(userRepository.findAll());
+        System.out.println(userRepository.findByLastNameContainingIgnoreCase("last_name"));
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO USERS (ID, EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
 VALUES (1, 'user@gmail.com', 'User', 'Last_Name', 'qwerty'),
-       (2, 'admin@gmail.com', 'Admin', 'Last_Name', 'qwerty');
+       (2, 'admin@gmail.com', 'Admin', 'L_Name', 'qwerty');
 
 INSERT INTO USER_ROLE (ROLE, USER_ID)
 VALUES ('ROLE_USER', 1),
