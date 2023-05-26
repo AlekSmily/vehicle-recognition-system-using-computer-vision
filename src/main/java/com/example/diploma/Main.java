28797c58e1141bd5b07902ca17ec5e9c65ae2e67
AlekSmily <tutun228@gmail.com>
2023-05-26 19:36:10 +0300

    add error handling

--- src/main/java/com/example/diploma/Main.java
+++ src/main/java/com/example/diploma/Main.java
@@ -20,9 +20,6 @@ public class Main implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(userRepository.findAll());
-        System.out.println(userRepository.findByLastNameContainingIgnoreCase("last_name"));
-        System.out.println(userRepository.findByEmailIgnoreCase("admin@gmail.com"));
-        System.out.println(vehicleRepository.findByLicensePlate("АА5508ЕР"));
+        System.out.println("Hello!");
     }
 }
